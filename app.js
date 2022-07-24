bb8e576388b034f06b1df215b81c02e4e3486f88
Ben Attali <ben.attali8@gmail.com>
2022-07-23 22:07:59 -0400

    initialize app

---
+++ app.js
@@ -0,0 +1,21 @@
+const { App } = require('@slack/bolt');
+require('dotenv').config()
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+});
+
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
+
+(async () => {
+  // Start the app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
